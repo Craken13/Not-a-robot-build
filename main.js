69cb4ba64dbbb99839c23048d5291964e2f8a3a7
Craken13 <49389973+Craken13@users.noreply.github.com>
2021-04-22 21:32:49 +0200

    Generated random hexcolor to be clicked

--- main.js
+++ main.js
@@ -9,13 +9,16 @@ const elements = document.querySelectorAll(".element");
 const selectColor = document.getElementById("SelectColor");
 // selectColor = document.querySelector("#SelectColor");
 
+//Assign color to elements
 elements.forEach(function(element) {
     const color = getRandomColor();
 
     element.style.backgroundColor = color;
     element.innerHTML = color;
+    selectColor.innerHTML = color;
 });
 
+//Generate Random color func
 function getRandomColor() {
     const letter = "0123456789ABCDEF";
     let color = "#";
@@ -24,3 +27,12 @@ function getRandomColor() {
     }
     return color;
 }
+
+//Check if right color
+elements.forEach(function(element) {
+    element.addEventListener("click", function() {
+        if (element.innerHTML === selectColor.innerHTML) {
+            alert("You are human!");
+        } else {}
+    });
+});
