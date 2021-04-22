e0f79e001858eaa7a0802d85d0cb65ad80348ac4
Craken13 <49389973+Craken13@users.noreply.github.com>
2021-04-22 21:27:53 +0200

    Assigned colours and hex colour codes to the generated squares

--- main.js
+++ main.js
@@ -11,13 +11,16 @@ const selectColor = document.getElementById("SelectColor");
 
 elements.forEach(function(element) {
     const color = getRandomColor();
+
+    element.style.backgroundColor = color;
+    element.innerHTML = color;
 });
 
 function getRandomColor() {
     const letter = "0123456789ABCDEF";
     let color = "#";
     for (let i = 0; i < 6; i++) {
-        color += letter[Math.floor(math.random() * 16)];
+        color += letter[Math.floor(Math.random() * 16)];
     }
     return color;
 }
